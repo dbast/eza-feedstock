0c79bff1a01c315ac5e9e091febd91195706acfb
Daniel Bast <2790401+dbast@users.noreply.github.com>
2026-08-26 16:05:38 +0200

    Enable win-arm64, linux-riscv64, pixi

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,15 +2,18 @@
 
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
+set CARGO_EXTRA_ARGS=
+if %target_platform% neq %build_platform% (
+  if %target_platform%==win-arm64 (
+    set CARGO_EXTRA_ARGS=--target aarch64-pc-windows-msvc
+  )
+  if %target_platform%==win-64 (
+    set CARGO_EXTRA_ARGS=--target x86_64-pc-windows-msvc
+  )
+)
 
 :: check licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 :: build
-cargo install --locked  --root "%LIBRARY_PREFIX%" --path . --no-track || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
+cargo install --locked --root "%LIBRARY_PREFIX%" --path . --no-track %CARGO_EXTRA_ARGS% || exit 1
